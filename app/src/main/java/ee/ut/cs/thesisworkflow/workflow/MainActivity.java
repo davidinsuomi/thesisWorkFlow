8fc7caa77cd4eb1119977ce388172d972c4064de
weiding <davidinsuomi@gmail.com>
2015-03-28 20:11:28 +0200

    commit before make any changes

--- app/src/main/java/ee/ut/cs/thesisworkflow/workflow/MainActivity.java
+++ app/src/main/java/ee/ut/cs/thesisworkflow/workflow/MainActivity.java
@@ -154,7 +154,7 @@ public class MainActivity extends Activity {
         protected Void doInBackground(Void... params) {
 
             //testing the coap
-            CoapConnection coapConnection = new CoapConnection("DISCOVER","coap://localhost","");
+            CoapConnection coapConnection = new CoapConnection("GET","coap://localhost/coapIP","");
             coapConnection.Connect();
             return null;
         }
